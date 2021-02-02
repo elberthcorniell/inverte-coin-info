888a0eb85d265735c833b35e4ac48f1d7bde8d98
Elbert Corniell <elberthcorniell@gmail.com>
2021-02-02 12:33:48 -0400

    Router created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-function App() {
-  return (
-    <div>
-      <h1>Hello world!</h1>
-    </div>
-  );
-}
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Home, Coin } from './containers';
+
+const App = () => (
+    <BrowserRouter>
+        <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/coin/:coin" component={Coin} />
+        </Switch>
+    </BrowserRouter>
+);
 
 export default App;
