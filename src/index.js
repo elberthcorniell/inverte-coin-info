902d16961c1b443507ce01b56c91073140212b60
Elbert Corniell <elberthcorniell@gmail.com>
2021-02-03 11:45:18 -0400

    Wrapped index with <Provider />

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react';
 import { render } from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './reducers';
 
 render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
   document.getElementById('root')
 );
