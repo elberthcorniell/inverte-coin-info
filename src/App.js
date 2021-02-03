3704fb8cd2cbbde179d6408f5c14c408e3aa614a
Elbert Corniell <elberthcorniell@gmail.com>
2021-02-03 11:42:48 -0400

    Created Router and App wraper

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 import { Home, Coin } from './containers';
 
 const App = () => (
     <BrowserRouter>
-        <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/coin/:coin" component={Coin} />
-        </Switch>
+        <NavBar />
+        <div className="container">
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/coin/:coin" component={Coin} />
+            </Switch>
+        </div>
+        <Footer />
     </BrowserRouter>
 );
 
